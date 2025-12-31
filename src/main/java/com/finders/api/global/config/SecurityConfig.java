0bc21c6206fc145a63bc6879e450e80425908acc
wldy4627 <wldy4627@gmail.com>
2025-12-31 12:18:15 +0900

    feat: CORS 에러 처리 주소 추가 (#5)

--- src/main/java/com/finders/api/global/config/SecurityConfig.java
+++ src/main/java/com/finders/api/global/config/SecurityConfig.java
@@ -71,7 +71,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "https://finders-chi.vercel.app",
+                "http://localhost:3000",
+                "http://localhost:5173"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
