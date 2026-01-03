2530da9cfde80454601139a678d7df9b84fec56a
young <ljy040206@gmail.com>
2026-01-04 00:34:25 +0900

    fix: 변수명 및 코드 수정 (#15)

--- src/main/java/com/finders/api/domain/community/entity/PostImage.java
+++ src/main/java/com/finders/api/domain/community/entity/PostImage.java
@@ -1,4 +1,32 @@
 package com.finders.api.domain.community.entity;
 
-public class post_image {
+import com.finders.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "post_image")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false, length = 500)
+    private String imageUrl;
+
+    private Integer width;
+
+    private Integer height;
+
+    @Column(name = "display_order")
+    private Integer displayOrder;
 }
--- src/main/java/com/finders/api/domain/community/entity/PostLike.java
+++ src/main/java/com/finders/api/domain/community/entity/PostLike.java
@@ -6,14 +6,11 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
-@Table(name = "post")
+@Table(name = "post_like")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-
-public class post_like extends BaseEntity {
+public class PostLike extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,9 +18,9 @@ public class post_like extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
-    private Post post_id;
+    private Post post;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id", nullable = false)
+//    private Member member;
 }
