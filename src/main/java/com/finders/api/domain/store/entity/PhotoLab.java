98f0aaf6c46377c3503173eec4c2ff0e55dbe645
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-03 18:51:02 +0900

    feat: photo_lab_inage 엔티티 추가

--- src/main/java/com/finders/api/domain/store/entity/PhotoLab.java
+++ src/main/java/com/finders/api/domain/store/entity/PhotoLab.java
@@ -4,10 +4,7 @@ import com.finders.api.domain.store.enums.PhotoLabStatus;
 import com.finders.api.global.entity.BaseEntity;
 //import com.finders.api.domain.member.entity.Member;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -22,8 +19,8 @@ import java.math.BigDecimal;
 )
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PhotoLab extends BaseEntity {
 
     @Id
--- src/main/java/com/finders/api/domain/store/entity/PhotoLabImage.java
+++ src/main/java/com/finders/api/domain/store/entity/PhotoLabImage.java
@@ -1,4 +1,46 @@
 package com.finders.api.domain.store.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "photo_lab_image",
+        indexes = {
+                @Index(name = "idx_lab_image", columnList = "photo_lab_id, is_main")
+        }
+)
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
 public class PhotoLabImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "photo_lab_id", nullable = false)
+    private PhotoLab photolab;
+
+    @Column(name = "image_url", nullable = false, length = 500)
+    private String imageUrl;
+
+    @Builder.Default
+    @Column(name = "display_order", nullable = false)
+    private Integer displayOrder = 0;
+
+    @Builder.Default
+    @Column(name = "is_main", nullable = false)
+    private boolean isMain = false;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
