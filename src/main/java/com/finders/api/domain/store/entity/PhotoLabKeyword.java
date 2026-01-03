82306bafc628d331ca1706dd9ccde4c1db156b14
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-03 19:21:17 +0900

    feat: 키워드, 공지사항 엔티티 추가

--- src/main/java/com/finders/api/domain/store/entity/PhotoLabKeyword.java
+++ src/main/java/com/finders/api/domain/store/entity/PhotoLabKeyword.java
@@ -1,4 +1,40 @@
 package com.finders.api.domain.store.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "photo_lab_keyword",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_lab_keyword", columnNames = {"photo_lab_id", "keyword"})
+        }
+)
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
 public class PhotoLabKeyword {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * FK → photo_lab(id)
+     */
+    @ManyToOne
+    @JoinColumn(name = "photo_lab_id", nullable = false)
+    private PhotoLab photoLab;
+
+    @Column(nullable = false, length = 50)
+    private String keyword;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
