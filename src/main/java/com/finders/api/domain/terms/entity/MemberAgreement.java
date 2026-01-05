f7984a0ab28b07439134f0e1c6b10b955edc42dc
wldy4627 <wldy4627@gmail.com>
2026-01-05 14:35:03 +0900

    feat: 약관 및 약관 동의 엔티티 추가 (#30)

---
+++ src/main/java/com/finders/api/domain/terms/entity/MemberAgreement.java
@@ -0,0 +1,41 @@
+package com.finders.api.domain.terms.entity;
+
+import com.finders.api.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.lang.reflect.Member;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "member_agreement",
+        indexes = {
+                @Index(name = "idx_agreement_member", columnList = "member_id"),
+                @Index(name = "idx_agreement_terms", columnList = "terms_id")
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberAgreement extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id", nullable = false)
+    private Terms terms;
+
+    @Column(nullable = false)
+    private boolean isAgreed;
+
+    @Column(nullable = false)
+    private LocalDateTime agreedAt;
+}
