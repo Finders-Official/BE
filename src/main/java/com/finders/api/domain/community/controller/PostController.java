86781566c32475dda470f28e441cb60aff65847b
young <ljy040206@gmail.com>
2026-01-06 18:15:40 +0900

    fix: Post controller 경로 수정 및 entity 수정 (#34)

--- src/main/java/com/finders/api/domain/community/controller/PostController.java
+++ src/main/java/com/finders/api/domain/community/controller/PostController.java
@@ -10,62 +10,62 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Community", description = "사진 수다 관련 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/posts")
 public class PostController {
 
     // 게시글 관련
     @Operation(summary = "피드 목록 조회", description = "전체 게시글 목록을 최신순으로 조회합니다.")
-    @GetMapping("/posts")
+    @GetMapping
     public ApiResponse<String> getPosts() {
         return ApiResponse.success(SuccessCode.POST_FOUND, "피드 목록 조회 성공");
     }
 
     @Operation(summary = "게시물 작성", description = "게시글 등록 API입니다.")
-    @PostMapping("/posts")
+    @PostMapping
     public ApiResponse<String> createPost() {
         return ApiResponse.success(SuccessCode.POST_CREATED, "게시물 작성 성공");
     }
 
     @Operation(summary = "게시물 상세 조회", description = "특정 ID의 게시글 상세 정보를 조회합니다.")
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/{postId}")
     public ApiResponse<String> getPostDetail(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.POST_FOUND, postId + "번 게시물 상세 조회 성공");
     }
 
     @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
-    @DeleteMapping("/posts/{postId}")
+    @DeleteMapping("/{postId}")
     public ApiResponse<String> deletePost(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.OK, postId + "번 게시글 삭제 성공");
     }
 
     // 댓글 관련
     @Operation(summary = "게시물 댓글 조회", description = "특정 게시글에 달린 댓글 목록을 조회합니다.")
-    @GetMapping("/posts/{postId}/comments")
+    @GetMapping("/{postId}/comments")
     public ApiResponse<String> getComments(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.OK, postId + "번 게시물의 댓글 목록 조회 성공");
     }
 
     @Operation(summary = "게시물 댓글 작성", description = "특정 게시글에 새로운 댓글을 남깁니다.")
-    @PostMapping("/posts/{postId}/comments")
+    @PostMapping("/{postId}/comments")
     public ApiResponse<String> createComment(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.CREATED, postId + "번 게시물에 댓글 작성 성공");
     }
 
     @Operation(summary = "게시물 댓글 삭제", description = "댓글을 삭제합니다.")
-    @DeleteMapping("/comments/{commentId}")
-    public ApiResponse<String> deleteComment(@PathVariable Long commentId) {
+    @DeleteMapping("/{postId}/comments/{commentId}")
+    public ApiResponse<String> deleteComment(@PathVariable Long postId ,@PathVariable Long commentId) {
         return ApiResponse.success(SuccessCode.OK, commentId + "번 댓글 삭제 성공");
     }
 
     // 좋아요 관련
     @Operation(summary = "게시물 좋아요", description = "게시글에 좋아요를 누릅니다.")
-    @PostMapping("/posts/{postId}/likes")
+    @PostMapping("/{postId}/likes")
     public ApiResponse<String> addLike(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.OK, postId + "번 게시물 좋아요 성공");
     }
 
     @Operation(summary = "게시물 좋아요 취소", description = "게시글 좋아요를 취소합니다.")
-    @DeleteMapping("/posts/{postId}/likes")
+    @DeleteMapping("/{postId}/likes")
     public ApiResponse<String> cancelLike(@PathVariable Long postId) {
         return ApiResponse.success(SuccessCode.OK, postId + "번 게시물 좋아요 취소 성공");
     }
