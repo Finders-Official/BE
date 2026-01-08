c78ffc1d04f67760938f4229c4fdd0049b3c69b2
wldy4627 <wldy4627@gmail.com>
2026-01-09 00:03:30 +0900

    fix: 미사용 메서드 삭제 (#39)

--- src/main/java/com/finders/api/domain/member/repository/SocialAccountRepository.java
+++ src/main/java/com/finders/api/domain/member/repository/SocialAccountRepository.java
@@ -4,13 +4,8 @@ import com.finders.api.domain.member.entity.SocialAccount;
 import com.finders.api.domain.member.enums.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
     Optional<SocialAccount> findByProviderAndProviderId(SocialProvider socialProvider, String providerId);
-
-    List<SocialAccount> findAllByMemberId(Long memberId);
-
-    boolean existsByMemberIdAndProvider(Long memberId, SocialProvider socialProvider);
 }
