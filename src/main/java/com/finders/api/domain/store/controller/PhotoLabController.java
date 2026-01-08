069e50e20e2b0ac7a19606426a1235d96983fcdd
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-08 19:59:45 +0900

    feat: 현상소 기본정보 등록 및 현상소 등록 api 구현

---
+++ src/main/java/com/finders/api/domain/store/controller/PhotoLabController.java
@@ -0,0 +1,35 @@
+package com.finders.api.domain.store.controller;
+
+import com.finders.api.domain.store.dto.PhotoLabRequest;
+import com.finders.api.domain.store.dto.PhotoLabResponse;
+import com.finders.api.domain.store.service.PhotoLabService;
+import com.finders.api.global.response.ApiResponse;
+import com.finders.api.global.response.SuccessCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "PhotoLab", description = "현상소 등록/관리 API")
+@RestController
+@RequestMapping("/photo-labs")
+@RequiredArgsConstructor
+public class PhotoLabController {
+
+    private final PhotoLabService photoLabService;
+
+    @Operation(summary = "현상소 기본사항 등록 API")
+    @PostMapping
+    public ApiResponse<PhotoLabResponse.Create> createPhotoLab(
+            @Valid @RequestBody PhotoLabRequest.Create request
+    ) {
+        return ApiResponse.success(
+                SuccessCode.CREATED,
+                photoLabService.createPhotoLab(request)
+        );
+    }
+}
