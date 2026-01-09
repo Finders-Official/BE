2d1811481c02548e94d21cac85ffe6a61ed4b406
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-09 19:57:25 +0900

    fix: 코드 컨벤션에 따른 MemberOwnerRepo 추가 및 서비스 구현

---
+++ src/main/java/com/finders/api/domain/member/repository/MemberOwnerRepository.java
@@ -0,0 +1,10 @@
+﻿package com.finders.api.domain.member.repository;
+
+import com.finders.api.domain.member.entity.MemberOwner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * MemberOwner Repository
+ */
+public interface MemberOwnerRepository extends JpaRepository<MemberOwner, Long> {
+}
