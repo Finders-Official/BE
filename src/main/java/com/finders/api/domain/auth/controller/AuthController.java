a722c035a3a49740d3acb42fa2f8ef0ed580e054
wldy4627 <wldy4627@gmail.com>
2026-01-10 02:54:38 +0900

    chore: swagger에 컨트롤러, API 설명 추가 (#39)

--- src/main/java/com/finders/api/domain/auth/controller/AuthController.java
+++ src/main/java/com/finders/api/domain/auth/controller/AuthController.java
@@ -3,13 +3,13 @@ package com.finders.api.domain.auth.controller;
 import com.finders.api.domain.auth.dto.AuthRequest;
 import com.finders.api.domain.auth.service.command.AuthCommandService;
 import com.finders.api.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "인증(Auth)", description = "인증 및 토큰 관련 API")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -17,6 +17,10 @@ public class AuthController {
 
     private final AuthCommandService authCommandService;
 
+    @Operation(
+            summary = "소셜 로그인",
+            description = "유저용 소셜 로그인 요청입니다."
+    )
     @PostMapping("/social/login")
     public ApiResponse<?> socialLogin(
             @Valid @RequestBody AuthRequest.SocialLogin request
