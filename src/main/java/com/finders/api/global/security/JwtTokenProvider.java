68112992bb48a94006b66d1c70b1f1c5d1ed6ab5
wldy4627 <wldy4627@gmail.com>
2026-01-10 02:47:35 +0900

    feat: JWT 인증 객체 생성 및 토큰 검증 로직 구현 (#39)

--- src/main/java/com/finders/api/global/security/JwtTokenProvider.java
+++ src/main/java/com/finders/api/global/security/JwtTokenProvider.java
@@ -1,13 +1,20 @@
 package com.finders.api.global.security;
 
-import com.finders.api.domain.member.entity.MemberUser;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @Component
 public class JwtTokenProvider {
 
@@ -27,6 +34,7 @@ public class JwtTokenProvider {
 
         return Jwts.builder()
                 .subject(String.valueOf(memberId))
+                .claim("type", "ACCESS")
                 .claim("role", role)
                 .issuedAt(now)
                 .expiration(exp)
@@ -45,5 +53,58 @@ public class JwtTokenProvider {
                 .expiration(exp)
                 .signWith(secretKey)
                 .compact();
+    }
+
+    // 인증 & 검증 로직
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+
+        String memberId = claims.getSubject();
+        String role = claims.get("role", String.class);
+
+        if (role == null) {
+            // RefreshToken 등으로 접근했을 경우 예외 처리
+            throw new JwtException("권한 정보가 없는 토큰입니다.");
+        }
+
+        String authority = "ROLE_" + role;
+
+        UserDetails principal = new User(String.valueOf(memberId), "",
+                List.of(new SimpleGrantedAuthority(authority)));
+
+        return new UsernamePasswordAuthenticationToken(principal, token, principal.getAuthorities());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            return "ACCESS".equals(claims.get("type"));
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
     }
 }
