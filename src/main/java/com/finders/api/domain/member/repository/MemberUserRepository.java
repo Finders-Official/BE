8e0763b074feec1a69a1fb818e76c5830ddcaef0
wldy4627 <wldy4627@gmail.com>
2026-01-10 02:48:25 +0900

    feat: member 관련 레포지토리 추가 (#39)

--- src/main/java/com/finders/api/domain/member/repository/MemberUserRepository.java
+++ src/main/java/com/finders/api/domain/member/repository/MemberUserRepository.java
@@ -4,4 +4,6 @@ import com.finders.api.domain.member.entity.MemberUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberUserRepository extends JpaRepository<MemberUser, Long> {
+
+    boolean existsByNickname(String nickname);
 }
