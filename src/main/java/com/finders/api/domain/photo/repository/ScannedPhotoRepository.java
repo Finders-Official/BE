2334cb32fd61006a56eef8c7579d42c6c19ed51f
이승주 <eun07213@gmail.com>
2026-01-14 17:08:24 +0900

    feat: 현상,스캔 레포지토리 구현

--- src/main/java/com/finders/api/domain/photo/repository/ScannedPhotoRepository.java
+++ src/main/java/com/finders/api/domain/photo/repository/ScannedPhotoRepository.java
@@ -1,7 +1,20 @@
 package com.finders.api.domain.photo.repository;
 
 import com.finders.api.domain.photo.entity.ScannedPhoto;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ScannedPhotoRepository extends JpaRepository<ScannedPhoto, Long> {
+    @Query("""
+        select sp
+        from ScannedPhoto sp
+        where sp.order.id in :orderIds
+        order by sp.displayOrder asc
+    """)
+    List<ScannedPhoto> findByOrderIdInOrderByDisplayOrderAsc(List<Long> orderIds);
+
+    Slice<ScannedPhoto> findByOrderIdOrderByDisplayOrderAsc(Long orderId, Pageable pageable);
 }
