7c8cb9783bc31169dc22ecc9a97cae9c8fd7042a
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-15 23:38:43 +0900

    fix: @AuthenticatinPrincipal 적용

--- src/main/java/com/finders/api/domain/store/service/command/PhotoLabFavoriteCommandService.java
+++ src/main/java/com/finders/api/domain/store/service/command/PhotoLabFavoriteCommandService.java
@@ -1,11 +1,10 @@
 package com.finders.api.domain.store.service.command;
 
-import com.finders.api.domain.member.entity.MemberUser;
 import com.finders.api.domain.store.dto.response.PhotoLabFavoriteResponse;
 
 public interface PhotoLabFavoriteCommandService {
 
-    PhotoLabFavoriteResponse.Status addFavorite(Long photoLabId, MemberUser memberUser);
+    PhotoLabFavoriteResponse.Status addFavorite(Long photoLabId, Long memberId);
 
-    PhotoLabFavoriteResponse.Status removeFavorite(Long photoLabId, MemberUser memberUser);
+    PhotoLabFavoriteResponse.Status removeFavorite(Long photoLabId, Long memberId);
 }
