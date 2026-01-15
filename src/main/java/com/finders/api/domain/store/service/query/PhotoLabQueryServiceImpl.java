bece32e1f646bed601623ad25835de31940c4152
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-16 02:08:04 +0900

    fix: 사용하지 않는 import문 삭제

--- src/main/java/com/finders/api/domain/store/service/query/PhotoLabQueryServiceImpl.java
+++ src/main/java/com/finders/api/domain/store/service/query/PhotoLabQueryServiceImpl.java
@@ -22,7 +22,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
