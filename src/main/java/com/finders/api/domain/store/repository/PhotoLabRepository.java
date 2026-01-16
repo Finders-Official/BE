2b28d6468956db28121cc8ba44c3d052c6478dbe
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-01-17 00:35:50 +0900

    feat: 현상소 상세 페이지 api 구현

--- src/main/java/com/finders/api/domain/store/repository/PhotoLabRepository.java
+++ src/main/java/com/finders/api/domain/store/repository/PhotoLabRepository.java
@@ -1,16 +1,19 @@
-package com.finders.api.domain.store.repository;
+﻿package com.finders.api.domain.store.repository;
 
 import com.finders.api.domain.store.entity.PhotoLab;
+import com.finders.api.domain.store.enums.PhotoLabStatus;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface PhotoLabRepository extends JpaRepository<PhotoLab, Long> {
     List<PhotoLab> findTop8ByOrderByReservationCountDescIdAsc();
+
+    Optional<PhotoLab> findByIdAndStatus(Long id, PhotoLabStatus status);
 
     // 커뮤니티 현상소 검색
     @Query("SELECT pl FROM PhotoLab pl " +
