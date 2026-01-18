105ab8eb092d1094d838fdef8d2559a68d845feb
이승주 <eun07213@gmail.com>
2026-01-18 22:23:37 +0900

    feat: 포토 관련 레포지토리 구현(인화)

--- src/main/java/com/finders/api/domain/photo/repository/DevelopmentOrderRepository.java
+++ src/main/java/com/finders/api/domain/photo/repository/DevelopmentOrderRepository.java
@@ -1,9 +1,11 @@
 package com.finders.api.domain.photo.repository;
 
 import com.finders.api.domain.photo.entity.DevelopmentOrder;
+import com.finders.api.domain.photo.enums.DevelopmentOrderStatus;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -29,7 +31,7 @@ public interface DevelopmentOrderRepository extends JpaRepository<DevelopmentOrd
         where d.user.id = :memberId
         """
     )
-    Page<DevelopmentOrder> findMyOrdersWithPhotoLab(@Param("memberId") Long memberId, Pageable pageable);
+    Slice<DevelopmentOrder> findMyOrdersWithPhotoLab(@Param("memberId") Long memberId, Pageable pageable);
 
     boolean existsByIdAndUser_Id(Long id, Long memberId);
 
@@ -44,4 +46,5 @@ public interface DevelopmentOrderRepository extends JpaRepository<DevelopmentOrd
             @Param("orderId") Long orderId,
             @Param("memberId") Long memberId
     );
+    Optional<DevelopmentOrder> findByUserIdAndStatusNot(Long userId, DevelopmentOrderStatus status);
 }
--- src/main/java/com/finders/api/domain/photo/repository/PrintOrderRepository.java
+++ src/main/java/com/finders/api/domain/photo/repository/PrintOrderRepository.java
@@ -1,9 +1,17 @@
 package com.finders.api.domain.photo.repository;
 
 import com.finders.api.domain.photo.entity.PrintOrder;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PrintOrderRepository extends JpaRepository<PrintOrder, Long> {
     Optional<PrintOrder> findByIdAndUserId(Long id, Long memberId);
+
+    @Query("select po from PrintOrder po join fetch po.developmentOrder d where d.id in :devOrderIds")
+    List<PrintOrder> findByDevelopmentOrderIdIn(@Param("devOrderIds") List<Long> devOrderIds);
+
+    Optional<PrintOrder> findByDevelopmentOrderId(Long developmentOrderId);
 }
