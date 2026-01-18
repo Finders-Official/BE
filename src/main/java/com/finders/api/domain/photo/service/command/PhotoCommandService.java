2d8a09e274ee92aa07cb8becc5bffe1044c8bb19
이승주 <eun07213@gmail.com>
2026-01-18 22:22:46 +0900

    feat: 유저 포토 커맨드 서비스 API 추가(인화 안 함 확정)

--- src/main/java/com/finders/api/domain/photo/service/command/PhotoCommandService.java
+++ src/main/java/com/finders/api/domain/photo/service/command/PhotoCommandService.java
@@ -4,5 +4,8 @@ import com.finders.api.domain.photo.dto.PhotoRequest;
 
 public interface PhotoCommandService {
     Long createPrintOrder(Long memberId, PhotoRequest.PrintQuote request);
+
     Long confirmDepositReceipt(Long memberId, Long printOrderId, PhotoRequest.DepositReceiptConfirm request);
+
+    Long skipPrint(Long memberId, Long developmentOrderId);
 }
