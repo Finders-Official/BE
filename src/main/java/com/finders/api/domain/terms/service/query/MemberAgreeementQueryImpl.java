72d64dabf752581cbb0eafb63e81806eb308ba48
wldy4627 <wldy4627@gmail.com>
2026-01-29 22:17:26 +0900

    refactor: MemberAgreementRepository 위치 조정 및 중복 제거 (#219)

---
+++ src/main/java/com/finders/api/domain/terms/service/query/MemberAgreeementQueryImpl.java
@@ -0,0 +1,20 @@
+package com.finders.api.domain.terms.service.query;
+
+import com.finders.api.domain.terms.enums.TermsType;
+import com.finders.api.domain.terms.repository.MemberAgreementRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberAgreeementQueryImpl implements MemberAgreementQueryService{
+    
+    private final MemberAgreementRepository memberAgreementRepository;
+
+    @Override
+    public boolean hasAgreedToTerms(Long memberId, TermsType type) {
+        return memberAgreementRepository.existsByMember_IdAndTerms_TypeAndIsAgreed(memberId, type, true);
+    }
+}
