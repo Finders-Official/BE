019009bd2fec832df2284961359913f029ae4102
wldy4627 <wldy4627@gmail.com>
2026-01-29 23:47:13 +0900

    refactor: MemberAgreementQueryServiceImpl로 파일명 변경 (#217)

--- src/main/java/com/finders/api/domain/terms/service/query/MemberAgreeementQueryServiceImpl.java
+++ src/main/java/com/finders/api/domain/terms/service/query/MemberAgreeementQueryServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class MemberAgreeementQueryImpl implements MemberAgreementQueryService{
+public class MemberAgreeementQueryServiceImpl implements MemberAgreementQueryService{
     
     private final MemberAgreementRepository memberAgreementRepository;
 
