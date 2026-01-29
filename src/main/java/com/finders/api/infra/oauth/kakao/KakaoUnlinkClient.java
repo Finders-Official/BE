0cf819d5811cb6f355f4871d9d47b6c4cf4adf00
wldy4627 <wldy4627@gmail.com>
2026-01-29 21:29:57 +0900

    feat: 카카오 어드민 API 기반 소셜 연동 해제 인프라 구축 (#219)

---
+++ src/main/java/com/finders/api/infra/oauth/kakao/KakaoUnlinkClient.java
@@ -0,0 +1,43 @@
+package com.finders.api.infra.oauth.kakao;
+
+import com.finders.api.domain.member.enums.SocialProvider;
+import com.finders.api.global.exception.CustomException;
+import com.finders.api.global.response.ErrorCode;
+import com.finders.api.infra.oauth.OAuthUnlinkClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Profile("!local")
+@RequiredArgsConstructor
+public class KakaoUnlinkClient implements OAuthUnlinkClient {
+
+    private final RestClient restClient;
+
+    @Value("${kakao.admin-key}")
+    private String adminKey;
+
+    @Override
+    public SocialProvider getProvider() {
+        return SocialProvider.KAKAO;
+    }
+
+    @Override
+    public void unlink(String providerId) {
+        restClient.post()
+                .uri("https://kapi.kakao.com/v1/user/unlink")
+                .header("Authorization", "KakaoAK " + adminKey)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body("target_id_type=user_id&target_id=" + providerId)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, (request, response) -> {
+                    throw new CustomException(ErrorCode.KAKAO_UNLINK_FAILED);
+                })
+                .toBodilessEntity();
+    }
+}
