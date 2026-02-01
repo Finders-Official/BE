c93b6cb2208a0e79fdfe051d2b53bae4a3878936
이승주 <eun07213@gmail.com>
2026-02-01 23:05:52 +0900

    fix: 예약 완료 조회 서비스 프런트 요청사항 반영

--- src/main/java/com/finders/api/domain/reservation/dto/ReservationResponse.java
+++ src/main/java/com/finders/api/domain/reservation/dto/ReservationResponse.java
@@ -78,6 +78,8 @@ public class ReservationResponse {
                     .latitude(reservation.getPhotoLab().getLatitude())
                     .longitude(reservation.getPhotoLab().getLongitude())
                     .estimatedCompletion(completedAt)
+                    .address(reservation.getPhotoLab().getAddress())
+                    .addressDetail(reservation.getPhotoLab().getAddressDetail())
                     .build();
         }
     }
