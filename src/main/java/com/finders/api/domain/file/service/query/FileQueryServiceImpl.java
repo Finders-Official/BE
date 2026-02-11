fde9acbe5bf76280cb9f2733074bcecce2fbae2f
wldy4627 <wldy4627@gmail.com>
2026-02-11 20:24:00 +0900

    refactor: GCS Storage 접근 권한 에러문 변경 (#471)

--- src/main/java/com/finders/api/domain/file/service/query/FileQueryServiceImpl.java
+++ src/main/java/com/finders/api/domain/file/service/query/FileQueryServiceImpl.java
@@ -24,13 +24,13 @@ public class FileQueryServiceImpl implements FileQueryService {
 
         // 공통 카테고리인지 확인
         if (!pathType.isCommon()) {
-            throw new CustomException(ErrorCode.STORAGE_UNAUTHORIZED);
+            throw new CustomException(ErrorCode.STORAGE_ACCESS_DENIED);
         }
 
         // 2. 공통 파일이므로 추출된 ID는 반드시 memberId여야 함
         Long pathId = pathType.extractId(objectPath);
         if (!memberId.equals(pathId)) {
-            throw new CustomException(ErrorCode.STORAGE_UNAUTHORIZED);
+            throw new CustomException(ErrorCode.STORAGE_ACCESS_DENIED);
         }
 
         return storageService.getSignedUrl(objectPath, SIGNED_URL_EXPIRY_MINUTES);
