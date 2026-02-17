46e0ec42560760f4641c505ffc7f4dc10ac54043
IISweetHeartII <sachi009955@gmail.com>
2026-02-18 04:29:34 +0900

    fix: Cloud Run prevent_destroy 제거 및 ignore_changes 최소화 (#486)

--- infra/cloudrun.tf
+++ infra/cloudrun.tf
@@ -50,9 +50,7 @@ resource "google_cloud_run_v2_service" "img_resizer" {
   }
 
   lifecycle {
-    prevent_destroy = true
     ignore_changes = [
-      template[0].containers[0].image,
       client,
       client_version,
     ]
